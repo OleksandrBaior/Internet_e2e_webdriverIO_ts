33475a3980b49ab365bacee1eca96fc8fb6eeb4d
Oleksandr Baior <43751093+OleksandrBaior@users.noreply.github.com>
2022-11-04 16:41:16 +0300

    Update wdio.conf.ts

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -50,11 +50,11 @@ export const config: Options.Testrunner = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/*.ts'
+//         './test/specs/**/*.ts'
         // 'test/specs/RemoveElements.test.ts'
         // 'test/specs/basicAuth.test.ts'
         // 'test/specs/BrokenImages.test.ts'
-        // 'test/specs/Checkboxes.test.ts'
+        'test/specs/Checkboxes.test.ts'
         // 'test/specs/ContextMenu.test.ts'
         // 'test/specs/disappearingElements.test.ts'
         // 'test/specs/DisappearingElements.test.ts'
